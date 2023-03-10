27da1890c3956678740a39be95452b4e80d0f6b6
pohjie <0xArlet@gmail.com>
2023-03-10 20:07:12 +0800

    Implement READ for category list

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,7 +1,13 @@
 const Category = require("../models/category");
 
 exports.category_list = (req, res) => {
-  res.send("NOT IMPLEMENTED");
+  Category.find()
+          .then((list_categories) => {
+            res.render("category_list", { title: "Category List", category_list: list_categories });
+          })
+          .catch((err) => {
+            return next(err);
+          })
 };
 
 exports.category_detail = (req, res) => {
