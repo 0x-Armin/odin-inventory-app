367da5f1f35f559321553c430b2d89d39a56759c
pohjie <0xArlet@gmail.com>
2023-03-10 11:39:09 +0800

    Fill in skeleton for controllers

---
+++ controllers/boardController.js
@@ -0,0 +1,33 @@
+const Board = require("../models/board");
+
+exports.board_list = (req, res) => {
+  res.send("NOT IMPLEMENTED: Board list");
+};
+
+exports.board_detail = (req, res) => {
+  res.send("NOT IMPLEMENTED: Board detail");
+};
+
+exports.borad_create_get = (req, res) => {
+  res.send("NOT IMPLEMENTED");
+};
+
+exports.board_create_post = (req, res) => {
+  res.send("NOT IMPLEMENTED");
+};
+
+exports.board_delete_get = (req, res) => {
+  res.send("NOT IMPLEMENTED");
+};
+
+exports.board_delete_post = (req, res) => {
+  res.send("NOT IMPLEMENTED");
+};
+
+exports.board_update_get = (req, res) => {
+  res.send("NOT IMPLEMENTED");
+};
+
+exports.board_update_post = (req, res) => {
+  res.send("NOT IMPLEMENTED");
+};
